8314e6bc5d002cec687a83c82d179b37be0a6f4f
sea413 <student@epicodus.com>
2016-01-22 11:47:24 -0800

    Add divisble by 5 and 15.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-var kant ="Ping"
 $(document).ready(function() {
 $("form#question").submit(function(event) {
 
@@ -8,13 +7,17 @@ for (var i = 1; i <= gabe; i++) {
 appendText.push(i);
 }
 //
-for (var i = 1; i < appendText.length; i++) {
+for (var i = 1; i <= appendText.length; i++) {
 if (i % 3 ===0) {
   appendText[i-1]= "ping";
 }
+if (i % 5 ===0) {
+  appendText[i-1]= "Pong";
 }
-var new_array = appendText.concat();
-// appendText = appendText.join("");
+if (i % 15 ===0) {
+  appendText[i-1]= "PingPong";
+}
+}
 $("#sth").append(appendText + "<br>" );
 event.preventDefault();
 });
