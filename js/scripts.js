46f102f7100c96aa1e5c52caea847fde8c3b14d6
sea413 <student@epicodus.com>
2016-02-04 17:31:37 -0800

    Completed revisions and have prepared for resubmission.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,31 @@
-function Pingpong (test,appendText){
-this.test = 0;
-this.appendText = [];
+function Pingpong (appendText){
+  this.appendText = [];
 }
 
 Pingpong.prototype.intselect = function(valueinteger){
-for (var i = 1; i <= valueinteger; i++) {
-this.appendText.push(i);
+  for (var i = 1; i <= valueinteger; i++) {
+    this.appendText.push(i);
 }
-for (var i = 1; i <= this.appendText.length; i++) {
-if (i % 3 ===0) {
-  this.appendText[i-1]= "Ping";
+  for (var i = 1; i <= this.appendText.length; i++) {
+    if (i % 3 ===0) {
+      this.appendText[i-1]= "Ping";
 }
-if (i % 5 ===0) {
-  this.appendText[i-1]= "Pong";
+    if (i % 5 ===0) {
+      this.appendText[i-1]= "Pong";
 
 }
-if (i % 15 ===0) {
-  this.appendText[i-1]= "PingPong";
-}
-return this.appendText;
-}
+    if (i % 15 ===0) {
+      this.appendText[i-1]= "PingPong";
+    }
+  }
 }
 $(document).ready(function() {
-$("form#question").submit(function(event) {
-var valueinteger = parseInt($("input#gaben").val());
-var sequence = new Pingpong();
-sequence.intselect(valueinteger);
+  $("form#question").submit(function(event) {
+    var valueinteger = parseInt($("input#gaben").val());
+    var sequence = new Pingpong();
+    sequence.intselect(valueinteger);
 
-$("#sth").html(sequence.appendText + "<br>" );
-event.preventDefault();
-});
+    $("#sth").html(sequence.appendText + "<br>" );
+    event.preventDefault();
+  });
 });
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,4 @@
 describe('Pingpong', function() {
-    this.timeout(700000000000000);
     it("will begin create parameters for the array set.", function(){
       var testObject = new Pingpong ();
       expect(testObject.appendText).to.eql([]);
@@ -9,37 +8,25 @@ describe('Pingpong', function() {
     expect(testObject.appendText).to.eql([]);
   });
 
-  it("Final testing.", function(done){
+  it("Testing to see if the formula can recognize the implementation of three as a varaiable", function(){
     var testObject = new Pingpong();
-        expect(testObject.intselect(3)).to.eql([1,2,3]);
-    //     setTimeout(function(done, 700000000000000){
-    // });
+        testObject.intselect(3);
+        expect(testObject.appendText).to.eql([1,2,"Ping"]);
+  });
+  it("Attempting to see if the program can correctly recognize 5 as a variable of Pong", function(){
+    var testObject = new Pingpong();
+        testObject.intselect(5);
+        expect(testObject.appendText).to.eql([1,2,"Ping",4,"Pong"]);
+  });
+  it("Attempting to see if the program can correctly recognize 15 as a variable of PingPong", function(){
+    var testObject = new Pingpong();
+        testObject.intselect(15);
+        expect(testObject.appendText).to.eql([1,2,"Ping",4,"Pong","Ping",7,8,"Ping","Pong",11,"Ping",13,14,"PingPong"]);
   });
   });
-  // });
-  // describe('Pingpong', function() {
-  // describe('#indexOf()', function() {
-  // it("Further experimentation required.", function(){
-  //   [1,2,3].indexOf(5).should.equal(-1);
-  // });
-  // });
-  // });
-  //   });
-  // it("is true when 5 is without a remainder it will apply a seperate conditional for the Pong Result", function() {
-  //   expect(dogwhistle(5)).to.equal(true);
-  // });
-  //
-  // it("is true when 15 is provided as a value for the first time it will provide a reaminder of zero and create the png value", function() {
-  //   expect(dogwhistle(15)).to.equal(true);
-  // });
-  //
-  // it("is true the wu tang clan has always been good", function() {
-  //     expect(dogwhistle(5)).to.equal(true);
-  // });
 
 describe('emu', function() {
   it("is true when array is formulated it will result in a state when a non integar will occupy 3n, 5n, and 15n in their respective postions.", function() {
-        expect([1, 2, "Ping"]).to.eql([1, 2, "Ping"]);
+    expect([1, 2, "Ping"]).to.eql([1, 2, "Ping"]);
   });
-  });
-// });
+});
